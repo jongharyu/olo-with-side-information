89180552e7fda31c948206eeeb565fca673003f4
Jon Ryu <jonryu@Jons-MacBook-Pro.local>
2021-04-24 22:20:21 -0700

    Refactor modules further based on one-step updates

--- regression.py
+++ regression.py
@@ -6,10 +6,20 @@ from ctw import ContextTree
 
 
 class OnlineLinearRegressionWithAbsoluteLoss:
-    def __init__(self):
-        self.losses = None
-        self.lin_losses = None
-        self.w = None
+    def __init__(self, dim):
+        self.dim = dim
+
+        self._losses = []  # cumulative loss
+        self._lin_losses = []  # linearized cumulative loss
+        self.w = np.zeros(self.dim)
+
+    @property
+    def losses(self):
+        return np.array(self._losses)
+
+    @property
+    def lin_losses(self):
+        return np.array(self._lin_losses)
 
     @staticmethod
     def loss(w, x, y):
@@ -35,8 +45,33 @@ class OnlineLinearRegressionWithAbsoluteLoss:
         """
         return -x if w @ x < y else x
 
-    def fit(self, X, y):
-        pass
+    def get_side_information(self):
+        return None
+
+    def get_action(self, h_t=None):
+        raise NotImplementedError
+
+    def update(self, g_t, h_t=None):
+        raise NotImplementedError
+
+    def fit(self, x_t, y_t):
+        h_t = self.get_side_information()
+        self.w = self.get_action(h_t)
+        g_t = self.subgradient(self.w, x_t, y_t)
+
+        # Incur loss
+        self._losses.append(self.loss(self.w, x_t, y_t))
+        self._lin_losses.append(g_t @ self.w)
+
+        self.update(g_t, h_t)
+
+    def fit_batch(self, X, y):
+        self.dim = X.shape[1]
+
+        for i in range(len(X)):
+            self.fit(X[i], y[i])
+
+        return self
 
     @property
     def cumulative_loss(self):
@@ -44,43 +79,33 @@ class OnlineLinearRegressionWithAbsoluteLoss:
 
 
 class OnlineGradientDescent(OnlineLinearRegressionWithAbsoluteLoss):
-    def __init__(self, lr_scale):
-        super().__init__()
+    def __init__(self, dim, lr_scale):
+        super().__init__(dim)
+        # hyperparameters
         self.lr_scale = lr_scale
+
+        # initialize variables
+        self.g_prev = None
 
     def lr(self, t):
         # time varying learning rate
         return self.lr_scale / np.sqrt(t)
 
-    def fit(self, X, y):
-        T, dim = X.shape
-        w = np.zeros(dim)
+    def get_action(self, h_t=None):
+        t = len(self._losses) + 1
+        if t > 1:
+            assert self.g_prev is not None
+            w = self.w - self.lr(t) * self.g_prev  # no projection since unconstrained
+            return w
+        else:
+            return self.w
 
-        losses = []  # cumulative loss
-        lin_losses = []  # linearized cumulative loss
-
-        for t in range(1, T+1):
-            # Receive data point and compute gradient
-            x_t, y_t = X[t - 1], y[t - 1]
-            g_t = self.subgradient(w, x_t, y_t)
-
-            # Incur loss
-            losses.append(self.loss(w, x_t, y_t))
-            lin_losses.append(g_t @ w)
-
-            # Update
-            w = w - self.lr(t) * g_t  # no projection since unconstrained
-
-        # Store results
-        self.losses = np.array(losses)
-        self.lin_losses = np.array(lin_losses)
-        self.w = w
-
-        return self
+    def update(self, g_t, h_t=None):
+        self.g_prev = g_t
 
 
 class DimensionFreeExponentiatedGradient(OnlineLinearRegressionWithAbsoluteLoss):
-    def __init__(self, a=1, L=1, delta=1):
+    def __init__(self, dim, a=1, L=1, delta=1):
         """
         Dimension-free Exponentiated Gradient (DFEG)
 
@@ -94,55 +119,38 @@ class DimensionFreeExponentiatedGradient(OnlineLinearRegressionWithAbsoluteLoss)
         delta
         L: Lipschitz constant
         """
-        super().__init__()
+        super().__init__(dim)
+        # hyperparameters
         self.a = a
         self.delta = delta
         self.L = L
 
-    def fit(self, X, y):
-        T, dim = X.shape
+        # initialize variables
+        self.w = np.zeros(self.dim)
+        self.th = np.zeros(self.dim)
+        self.H = self.delta
 
-        # initializing variables
-        th_t = np.zeros(dim)
-        H_t = self.delta
-        w = None
+    def get_action(self, h_t=None):
+        # Set parameters
+        self.H += self.L ** 2  # assumed ||x_t|| = 1; TODO: check if this requires to set normalize=True
+        alpha = self.a * np.sqrt(self.H)
+        beta = self.H ** (3 / 2)
 
-        losses = []  # cumulative loss
-        lin_losses = []  # linearized cumulative loss
+        # Set w
+        norm_th = np.linalg.norm(self.th)
+        if norm_th == 0:
+            w = np.zeros(self.dim)
+        else:
+            w = (self.th / norm_th) * (np.exp(norm_th / alpha) / beta)  # the EG step
 
-        for t in range(1, T + 1):
-            # Set w_t
-            H_t = H_t + self.L ** 2  # assumed ||x_t|| = 1; TODO: check if this requires to set normalize=True
-            alpha_t = self.a * np.sqrt(H_t)
-            beta_t = H_t ** (3 / 2)
+        return w
 
-            norm_th = np.linalg.norm(th_t)
-            if norm_th == 0:
-                w = np.zeros(dim)
-            else:
-                w = (th_t / norm_th) * (np.exp(norm_th / alpha_t) / beta_t)  # the EG step
-
-            # Receive data point and compute gradient
-            x_t, y_t = X[t - 1], y[t - 1]
-            g_t = self.subgradient(w, x_t, y_t)
-
-            # Incur loss
-            losses.append(self.loss(w, x_t, y_t))
-            lin_losses.append(g_t @ w)
-
-            # Update theta
-            th_t = th_t - g_t
-
-        # Store results
-        self.losses = np.array(losses)
-        self.lin_losses = np.array(lin_losses)
-        self.w = w
-
-        return self
+    def update(self, g_t, h_t=None):
+        self.th = self.th - g_t
 
 
 class AdaptiveNormal(OnlineLinearRegressionWithAbsoluteLoss):
-    def __init__(self, a=1, L=1, eps=1):
+    def __init__(self, dim, a=1, L=1, eps=1):
         """
         Adaptive Normal (AdaNormal)
 
@@ -156,88 +164,49 @@ class AdaptiveNormal(OnlineLinearRegressionWithAbsoluteLoss):
         L: Lipschitz constant
         eps
         """
-        super().__init__()
+        super().__init__(dim)
+        # hyperparameters
         self.a = a
         self.eps = eps
         self.L = L
 
-    def fit(self, X, y):
-        T, dim = X.shape
+        # initialize variables
+        self.th = np.zeros(dim)
 
-        # initializing variables
-        th_t = np.zeros(dim)
-        w = None
+    def get_action(self, h_t=None):
+        t = len(self._losses) + 1
 
-        losses = []  # cumulative loss
-        lin_losses = []  # linearized cumulative loss
+        # Set w_t
+        norm_th = np.linalg.norm(self.th)
+        if norm_th == 0:
+            w = np.zeros(self.dim)
+        else:
+            term1 = np.exp(((norm_th + self.L) ** 2) / (2 * self.a * (t + 1)))
+            term1 -= np.exp(((norm_th - self.L) ** 2) / (2 * self.a * (t + 1)))
+            term2 = (2 * self.L * (np.log(t + 2)) ** 2) ** (-1)
+            w = self.th * self.eps * term1 * term2 / norm_th  # the AdaNormal step
 
-        for t in range(1, T + 1):
-            # Set w_t
-            norm_th = np.linalg.norm(th_t)
-            if norm_th == 0:
-                w = np.zeros(dim)
-            else:
-                term1 = np.exp(((norm_th + self.L) ** 2) / (2 * self.a * (t + 1)))
-                term1 -= np.exp(((norm_th - self.L) ** 2) / (2 * self.a * (t + 1)))
-                term2 = (2 * self.L * (np.log(t + 2)) ** 2) ** (-1)
-                w = th_t * self.eps * term1 * term2 / norm_th  # the AdaNormal step
+        return w
 
-            # Receive data point and compute gradient
-            x_t, y_t = X[t - 1], y[t - 1]
-            g_t = self.subgradient(w, x_t, y_t)
-
-            # Incur loss
-            losses.append(self.loss(w, x_t, y_t))
-            lin_losses.append(g_t @ w)
-
-            # Update theta
-            th_t = th_t - g_t
-
-        # Store results
-        self.losses = np.array(losses)
-        self.lin_losses = np.array(lin_losses)
-        self.w = w
-
-        return self
+    def update(self, g_t, h_t=None):
+        self.th -= g_t
 
 
 class CoinBetting(OnlineLinearRegressionWithAbsoluteLoss):
-    def __init__(self, init_wealth=1):
-        super().__init__()
+    def __init__(self, dim, init_wealth=1):
+        super().__init__(dim)
         self.init_wealth = init_wealth
+        self.g_cum = np.zeros(self.dim)
 
-    def fit(self, X, y):
-        T, dim = X.shape
+    def get_action(self, h_t=None):
+        t = len(self._losses) + 1
+        v = self.g_cum / t  # vectorial KT betting
+        w = v * (self.init_wealth - self.lin_losses.sum())
 
-        # initialize variables
-        w = np.zeros(dim)
-        g_cum = np.zeros(dim)  # cumulative gradients
+        return w
 
-        losses = []  # cumulative loss
-        lin_losses = []  # linearized cumulative loss
-
-        for t in range(1, T + 1):
-            # Set w_t
-            v = g_cum / t  # vectorial KT betting
-            w = v * (self.init_wealth - sum(lin_losses))
-
-            # Receive data point and compute gradient
-            x_t, y_t = X[t - 1], y[t - 1]
-            g_t = self.subgradient(w, x_t, y_t)
-
-            # Incur loss
-            losses.append(self.loss(w, x_t, y_t))
-            lin_losses.append(g_t @ w)
-
-            # Update
-            g_cum = g_cum - g_t
-
-        # Store results
-        self.losses = np.array(losses)
-        self.lin_losses = np.array(lin_losses)
-        self.w = w
-
-        return self
+    def update(self, g_t, h_t=None):
+        self.g_cum -= g_t
 
 
 class Quantizer:
@@ -248,155 +217,116 @@ class Quantizer:
         return np.sign(self.quantizer_vector @ g + 1e-10).astype(int)
 
 
-class CoinBettingWithFixedDepthSideInformation(CoinBetting, Quantizer):
-    def __init__(self, init_wealth=1, depth=1, quantizer_vector=None):
+class CoinBettingWithQuantizedSideInformation(CoinBetting, Quantizer):
+    def __init__(self, dim, init_wealth=1, quantizer_vector=None):
         """
-        Implement coin betting with a fixed order Markov type side information with a binary quantizer
+        A placeholder for coin betting with side information with a binary quantizer
 
         Parameters
         ----------
+        dim
         init_wealth
         depth
         quantizer_vector
         """
-        CoinBetting.__init__(self, init_wealth)
+        CoinBetting.__init__(self, dim, init_wealth)
         Quantizer.__init__(self, quantizer_vector)
-        self.depth = depth
 
-    def fit(self, X, y):
-        T, dim = X.shape
+        # initialize parameters
+        self.counter = defaultdict(int)
+        self.g_cum = defaultdict(lambda: np.zeros(dim))  # cumulative gradients
 
-        # initialize variables
-        w = np.zeros(dim)
-        counter = defaultdict(int)
-        g_cum = defaultdict(lambda: np.zeros(dim))  # cumulative gradients
+    def get_action(self, h_t=None):
+        assert h_t is not None
+        v = self.g_cum[h_t] / (self.counter[h_t] + 1)  # vectorial KT betting
+        w = v * (self.init_wealth - self.lin_losses.sum())
+        return w
 
-        losses = []  # cumulative loss
-        lin_losses = []  # linearized cumulative loss
-
-        suffix = [1 for _ in range(self.depth)]  # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]]
-        for t in range(1, T + 1):
-            # Find side information
-            h_t = tuple(suffix)  # h_t = Q(g_{t-1}^{t-D})
-
-            # Set w_t
-            v = g_cum[h_t] / (counter[h_t] + 1)  # vectorial KT betting
-            w = v * (self.init_wealth - sum(lin_losses))
-
-            # Receive data point and compute gradient
-            x_t, y_t = X[t - 1], y[t - 1]
-            g_t = self.subgradient(w, x_t, y_t)
-
-            # Incur loss
-            losses.append(self.loss(w, x_t, y_t))
-            lin_losses.append(g_t @ w)
-
-            # Update
-            counter[h_t] += 1
-            g_cum[h_t] += -g_t  # accumulate -g since we are in a loss minimization framework
-            suffix = suffix[1:] + [self.quantize(g_t)]  # update the suffix
-
-        # Store results
-        self.losses = np.array(losses)
-        self.lin_losses = np.array(lin_losses)
-        self.w = w
-
-        return self
+    def update(self, g_t, h_t=None):
+        self.counter[h_t] += 1
+        self.g_cum[h_t] += -g_t  # accumulate -g since we are in a loss minimization framework
 
 
-class CoinBettingWithQuantizedHint(CoinBetting, Quantizer):
-    def __init__(self, init_wealth=1, quantizer_vector=None):
+class CoinBettingWithFixedDepthSideInformation(CoinBettingWithQuantizedSideInformation):
+    def __init__(self, dim, init_wealth=1, depth=1, quantizer_vector=None):
         """
         Implement coin betting with a fixed order Markov type side information with a binary quantizer
 
         Parameters
         ----------
+        dim
+        init_wealth
+        depth
+        quantizer_vector
+        """
+        CoinBettingWithQuantizedSideInformation.__init__(self, dim, init_wealth, quantizer_vector)
+        self.depth = depth
+        self.suffix = [1 for _ in range(self.depth)]  # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]]
+
+    def get_side_information(self):
+        h_t = tuple(self.suffix)  # h_t = Q(g_{t-1}^{t-D})
+        return h_t
+
+    def update(self, g_t, h_t=None):
+        super().update(g_t, h_t)
+        self.suffix = self.suffix[1:] + [self.quantize(g_t)]  # update the suffix
+
+
+class CoinBettingWithQuantizedHint(CoinBettingWithQuantizedSideInformation, Quantizer):
+    def __init__(self, dim, init_wealth=1, quantizer_vector=None):
+        """
+        Implement coin betting with a fixed order Markov type side information with a binary quantizer
+
+        Parameters
+        ----------
+        dim
         init_wealth
         quantizer_vector
         """
-        CoinBetting.__init__(self, init_wealth)
-        Quantizer.__init__(self, quantizer_vector)
+        CoinBettingWithQuantizedSideInformation.__init__(self, dim, init_wealth, quantizer_vector)
 
-    def fit(self, X, y):
-        T, dim = X.shape
+        # initialize parameters
+        self.counter = defaultdict(int)
+        self.g_cum = defaultdict(lambda: np.zeros(dim))  # cumulative gradients
 
-        # initialize variables
-        w = np.zeros(dim)
-        counter = defaultdict(int)
-        g_cum = defaultdict(lambda: np.zeros(dim))  # cumulative gradients
+    def get_side_information(self, x_t, y_t):
+        h_t = self.quantize(self.subgradient(self.w, x_t, y_t))
+        return h_t
 
-        losses = []  # cumulative loss
-        lin_losses = []  # linearized cumulative loss
+    def fit(self, x_t, y_t):
+        h_t = self.get_side_information(x_t, y_t)
+        self.w = self.get_action(h_t)
+        g_t = self.subgradient(self.w, x_t, y_t)
 
-        for t in range(1, T + 1):
-            # Receive data point and construct a hint (with blurry foresight)
-            x_t, y_t = X[t - 1], y[t - 1]
-            h_t = self.quantize(self.subgradient(w, x_t, y_t))
+        # Incur loss
+        self._losses.append(self.loss(self.w, x_t, y_t))
+        self._lin_losses.append(g_t @ self.w)
 
-            # Set w_t
-            v = g_cum[h_t] / (counter[h_t] + 1)  # vectorial KT betting
-            w = v * (self.init_wealth - sum(lin_losses))
-
-            # Compute gradient
-            g_t = self.subgradient(w, x_t, y_t)
-
-            # Incur loss
-            losses.append(self.loss(w, x_t, y_t))
-            lin_losses.append(g_t @ w)
-
-            # Update
-            counter[h_t] += 1
-            g_cum[h_t] += -g_t  # accumulate -g since we are in a loss minimization framework
-
-        # Store results
-        self.losses = np.array(losses)
-        self.lin_losses = np.array(lin_losses)
-        self.w = w
-
-        return self
+        self.update(g_t, h_t)
 
 
 class ContextTreeWeighting(OnlineLinearRegressionWithAbsoluteLoss, Quantizer):
-    def __init__(self, max_depth=1, alpha=.5, quantizer_vector=None, init_wealth=1):
-        super().__init__()
+    def __init__(self, dim, init_wealth=1, max_depth=1, alpha=.5, quantizer_vector=None):
+        OnlineLinearRegressionWithAbsoluteLoss.__init__(self, dim)
+        Quantizer.__init__(self, quantizer_vector)
+        self.init_wealth = init_wealth
         self.max_depth = max_depth
         self.alpha = alpha
-        self.init_wealth = init_wealth
-        Quantizer.__init__(self, quantizer_vector)
 
-    def fit(self, X, y):
-        T, dim = X.shape
+        self.suffix = [1 for _ in range(self.max_depth)]  # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]]
+        self.context_tree = ContextTree(max_depth=self.max_depth, alpha=self.alpha, dim=dim)
 
-        # initialize variables
-        w = np.zeros(dim)
-        context_tree = ContextTree(max_depth=self.max_depth, alpha=self.alpha, dim=dim)
+    def get_side_information(self):
+        h_t = tuple(self.suffix)  # h_t = Q(g_{t-1}^{t-D})
+        return h_t
 
-        losses = []  # cumulative loss
-        lin_losses = []  # linearized cumulative loss
+    def get_action(self, suffix=None):
+        v = self.context_tree.v_ctw(state=suffix)
+        w = v * (self.init_wealth - self.lin_losses.sum())
+        return w
 
-        # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]] (possibly at random)
-        suffix = [1 for _ in range(self.max_depth)]
-        for t in range(1, T + 1):
-            # Set w_t
-            v = context_tree.v_ctw(state=suffix)
-            w = v * (self.init_wealth - sum(lin_losses))
-
-            # Receive data point and compute gradient
-            x_t, y_t = X[t - 1], y[t - 1]
-            g_t = self.subgradient(w, x_t, y_t)
-
-            # Incur loss
-            losses.append(self.loss(w, x_t, y_t))
-            lin_losses.append(g_t @ w)
-
-            # Update
-            context_tree.update(state=suffix, g_new=-g_t)
-            suffix = suffix[1:] + [self.quantize(g_t)]  # update the suffix
-            suffix = suffix[:self.max_depth]  # to handle the degenerate case when max_depth=0
-
-        # Store results
-        self.losses = np.array(losses)
-        self.lin_losses = np.array(lin_losses)
-        self.w = w
-
-        return self
+    def update(self, g_t, suffix=None):
+        # Update
+        self.context_tree.update(state=suffix, g_new=-g_t)
+        self.suffix = list(suffix[1:]) + [self.quantize(g_t)]  # update the suffix
+        self.suffix = self.suffix[:self.max_depth]  # to handle the degenerate case when max_depth=0
