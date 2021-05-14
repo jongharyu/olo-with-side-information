c69bf789bde355d6d0dfc499b1e01cab8c3823f2
Jon Ryu <jonryu@Jons-MacBook-Pro.local>
2021-05-14 15:28:22 -0700

    Implement rescale; add temporal datasets and portfolios

--- datasets.py
+++ datasets.py
@@ -1,6 +1,8 @@
+import os
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 
 
@@ -23,14 +25,9 @@ class Dataset:
         return X_train, X_test, y_train, y_test
 
     @staticmethod
-    def normalize(X):
-        # instance normalization
-        return X / np.sqrt((X ** 2).sum(axis=1, keepdims=True))
-
-    @staticmethod
-    def batch_normalize(X):
-        # instance normalization
-        return X / np.max(np.sqrt((X ** 2).sum(axis=1, keepdims=True)))
+    def center(X):
+        mu = X.mean(axis=0, keepdims=True)
+        return X - mu, mu
 
     @staticmethod
     def standardize(X):
@@ -39,12 +36,35 @@ class Dataset:
         sigma[sigma == 0] = 1
         return (X - mu) / sigma, mu, sigma
 
-    def preprocess_attributes(self, X, standardize, normalize, batch_normalize):
-        assert not (normalize and batch_normalize), "Either instance normalization or batch normalization must be used"
+    @staticmethod
+    def rescale(X):
+        min = X.min(axis=0, keepdims=True)
+        max = X.max(axis=0, keepdims=True)
+        return (X - (min + max) / 2) / (max - min)
+
+    @staticmethod
+    def normalize(X, bias):
+        # instance normalization
+        X = X / np.sqrt((X ** 2).sum(axis=1, keepdims=True))
+        if bias:
+            X = np.hstack([X, np.ones((X.shape[0], 1))]) / np.sqrt(2)
+        return X
+
+    @staticmethod
+    def batch_normalize(X):
+        # instance normalization
+        return X / np.max(np.sqrt((X ** 2).sum(axis=1, keepdims=True)))
+
+    def preprocess_attributes(self, X, standardize, normalize, batch_normalize, rescale, bias):
+        assert not (normalize and batch_normalize), "Either one of instance normalization or batch normalization must be used"
+        assert not (standardize and rescale), "Either one of standardization or rescale must be used"
+
         if standardize:
-            X = self.standardize(X)[0]
+            X = self.center(X)[0]
+        if rescale:
+            X = self.rescale(X)
         if normalize:
-            X = self.normalize(X)
+            X = self.normalize(X, bias)
         if batch_normalize:
             X = self.batch_normalize(X)
 
@@ -58,18 +78,18 @@ class CpuSmall(Dataset):
     [1] https://www.cs.toronto.edu/~delve/data/comp-activ/desc.html
     [2] ftp://ftp.cs.toronto.edu/pub/neuron/delve/data/tarfiles/comp-activ.tar.gz
     """
-
-    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False):
+    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False, rescale=False, bias=False):
         super().__init__()
-        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize)
+        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize, rescale, bias)
         self.classification = False
         self.name = 'cpuSmall'
 
-    def load_and_preprocess(self, root, standardize, normalize, batch_normalize):
+    def load_and_preprocess(self, root, standardize, normalize, batch_normalize, rescale, bias):
         filename = '{}/data/cpuSmall/Prototask.data.gz'.format(root)
         df = pd.read_csv(filename, compression='gzip', header=None, sep=' ', error_bad_lines=False, engine='python')
         X, y = np.array(df[df.columns[:-1]]), np.array(df[df.columns[-1]])
-        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize)
+        X = np.log(X + (X.min(axis=0) == 0).astype(int))  # take logarithms
+        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize, rescale, bias)
 
         return X, y
 
@@ -80,14 +100,13 @@ class Houses(Dataset):
     ----------
     [1] http://lib.stat.cmu.edu/datasets/houses.zip
     """
-
-    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False):
+    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False, rescale=False, bias=False):
         super().__init__()
-        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize)
+        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize, rescale, bias)
         self.classification = False
         self.name = 'Houses'
 
-    def load_and_preprocess(self, root, standardize, normalize, batch_normalize):
+    def load_and_preprocess(self, root, standardize, normalize, batch_normalize, rescale, bias):
         filename = '{}/data/Houses/cadata_dataonly.txt'.format(root)
         df = pd.read_csv(filename, header=None, sep='  ', error_bad_lines=False, engine='python')
 
@@ -97,7 +116,7 @@ class Houses(Dataset):
         df[8] = pd.to_numeric(df[8])
 
         X, y = np.array(df[df.columns[1:]]), np.array(df[df.columns[0]])
-        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize)
+        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize, rescale, bias)
 
         return X, y
 
@@ -108,17 +127,144 @@ class YearPredictionMSD(Dataset):
     ----------
     [1] https://archive.ics.uci.edu/ml/datasets/YearPredictionMSD
     """
-
-    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False):
+    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False, rescale=False, bias=False):
         super().__init__()
-        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize)
+        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize, rescale, bias)
         self.classification = False
         self.name = 'YearPredictionMSD'
 
-    def load_and_preprocess(self, root, standardize, normalize, batch_normalize):
+    def load_and_preprocess(self, root, standardize, normalize, batch_normalize, rescale, bias):
         filename = '{}/data/YearPredictionMSD/YearPredictionMSD.txt.zip'.format(root)
         df = pd.read_csv(filename, compression='zip', header=None, sep=',', quotechar='"', error_bad_lines=False)
         X, y = np.array(df[df.columns[1:]]), np.array(df[df.columns[0]])
-        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize)
+        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize, rescale, bias)
 
         return X, y
+
+
+class BeijingPM2pt5(Dataset):
+    """
+    References
+    ----------
+    [1] https://archive.ics.uci.edu/ml/datasets/Beijing+PM2.5+Data
+    """
+    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False, rescale=False, bias=False):
+        super().__init__()
+        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize, rescale, bias)
+        self.classification = False
+        self.name = 'BeijingPM2.5'
+
+    def load_and_preprocess(self, root, standardize, normalize, batch_normalize, rescale, bias):
+        filename = '{}/data/BeijingPM2.5/PRSA_data_2010.1.1-2014.12.31.csv'.format(root)
+        df = pd.read_csv(filename, sep=',', error_bad_lines=False, engine='python')
+        del df['cbwd'], df['No'], df['year'], df['month'], df['day'], df['hour']
+        X = np.array(df[df.columns[1:]])[24:]
+        y = np.array(df[df.columns[:1]])[24:]
+
+        # logarithmic scaling
+        X[:, 3:] = np.log(1 + X[:, 3:])
+
+        # linear interpolating nan values
+        nan_start = -1
+        for i in range(len(y)):
+            if np.isnan(y[i]):
+                if nan_start < 0:  # if we are in the running nan values
+                    nan_start = i
+            else:
+                if nan_start >= 0:
+                    slope = (y[i] - y[nan_start - 1]) / (i - nan_start + 1)
+                    y[nan_start:i] = y[nan_start - 1] + np.array(
+                        [slope * (j - nan_start + 1) for j in range(nan_start, i)])
+                    nan_start = -1  # reset
+
+        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize, rescale, bias)
+
+        return X, y
+
+
+class MetroInterstateTrafficVolume(Dataset):
+    """
+    References
+    ----------
+    [1] https://archive.ics.uci.edu/ml/datasets/Metro+Interstate+Traffic+Volume
+    """
+    def __init__(self, root='.', standardize=False, normalize=True, batch_normalize=False, rescale=False, bias=False):
+        super().__init__()
+        self.X, self.y = self.load_and_preprocess(root, standardize, normalize, batch_normalize, rescale, bias)
+        self.classification = False
+        self.name = 'MetroInterstateTrafficVolume'
+
+    def load_and_preprocess(self, root, standardize, normalize, batch_normalize, rescale, bias):
+        filename = '{}/data/MetroInterstateTrafficVolume/Metro_Interstate_Traffic_Volume.csv'.format(root)
+        df = pd.read_csv(filename, sep=',', error_bad_lines=False, engine='python')
+
+        # delete unusable columns
+        del df['date_time'], df['weather_description']
+
+        # binarize holidays
+        holiday = np.array(df['holiday'])
+        holiday[~(holiday == 'None')] = 1
+        holiday[holiday == 'None'] = -1
+        df['holiday'] = holiday
+
+        # binarize weather
+        weather = np.array(df['weather_main'])
+        weather[~(weather == 'Clear')] = -1
+        weather[weather == 'Clear'] = 1
+        df['weather_main'] = weather
+
+        # binarize snow
+        df['snow_1h'][df['snow_1h'] > 0] = 1
+        df['snow_1h'][df['snow_1h'] == 0] = -1
+
+        # scale rain
+        scaled_rain = np.log(df['rain_1h'])
+        scaled_rain[scaled_rain == -np.inf] = scaled_rain[scaled_rain > -np.inf].min()
+        df['rain_1h'] = scaled_rain
+
+        # interpolate zero temperatures
+        df['temp'][11898:11902] = df['temp'][11897]
+        df['temp'][11946:11952] = df['temp'][11945]
+
+        # Set features and target
+        X = np.array(df[df.columns[:-1]]).astype(float)
+        y = np.array(df[df.columns[-1]]).astype(float)
+
+        X = self.preprocess_attributes(X, standardize, normalize, batch_normalize, rescale, bias)
+
+        return X, y
+
+
+class ExampleStocks(Dataset):
+    def __init__(self, root='.'):
+        super().__init__()
+
+        self.X, self.names = self.load_and_preprocess(root)
+        self.classification = False
+        self.name = 'ExampleStocks'
+
+    def load_and_preprocess(self, root):
+        stock_names = {'A': 'Agilent Technologies, Inc.',
+                       'B': 'Barnes Group Inc.',
+                       'C': 'Citigroup Inc.',
+                       'D': 'Dominion Energy, Inc.',
+                       'F': 'Ford Motor Company',
+                       'M': 'Macy\'s, Inc.',
+                       'S': 'Sprint Corporation',
+                       'T': 'AT&T Inc.',
+                       'X': 'United States Steel Corporation',
+                       'Y': 'Alleghany Corporation'}
+
+        path = '{}/data/Portfolio/'.format(root)
+        stocks = defaultdict(dict)
+        for filename in os.listdir(path):
+            if filename.endswith(".csv"):
+                df = pd.read_csv('{}/{}'.format(path, filename))
+                symbol = os.path.splitext(filename)[0]
+                stocks[symbol]['Name'] = stock_names[symbol]
+                stocks[symbol]['Relative'] = np.array(df['Close'] / df['Open'])
+
+        X = np.vstack([stocks[key]['Relative'] for key in sorted(stocks.keys())]).T
+        names = [stocks[key]['Name'] for key in sorted(stocks.keys())]
+
+        return X, names
---
+++ learners.py
@@ -0,0 +1,480 @@
+from collections import defaultdict
+
+import numpy as np
+from scipy.special import betaln, logsumexp
+
+from ctw import ContextTree
+
+
+class OnlineConvexOptimizer:
+    def __init__(self, dim, problem, init_w=None):
+        """
+        Subgradient based online convex optimization
+
+        Parameters
+        ----------
+        dim
+        """
+        self.dim = dim
+
+        assert hasattr(problem, 'compute_loss') and hasattr(problem, 'compute_subgradient')
+        self.problem = problem
+        self.compute_loss = problem.compute_loss
+        self.compute_subgradient = problem.compute_subgradient
+
+        # initialize statistics
+        self.init_w = np.zeros(self.dim) if init_w is None else init_w
+        self.w = self.init_w.copy()
+        self.cum_loss = 0
+        self.cum_reward = 0
+        self._losses = []  # list of losses
+        self._lin_losses = []  # list of linearized losses
+
+    def reset_stats(self):
+        self.w = np.zeros(self.dim)
+        self.cum_loss = 0
+        self.cum_reward = 0
+        self._losses = []
+        self._lin_losses = []
+        return self
+
+    def get_side_information(self, data=None):
+        return None
+
+    def get_action(self, h_t=None):
+        raise NotImplementedError
+
+    def update(self, g_t, h_t=None, data=None):
+        return self.update_losses(g_t, data)
+
+    def update_losses(self, g_t, data):
+        # compute and store losses
+        loss = self.compute_loss(self.w, data)
+        lin_loss = self.w @ g_t
+
+        self._losses.append(loss)
+        self.cum_loss += loss
+        self._lin_losses.append(lin_loss)
+        self.cum_reward -= lin_loss
+        return self
+
+    def fit_single(self, data):
+        # get side information
+        h_t = self.get_side_information(data)
+
+        # get action
+        self.w = self.get_action(h_t)
+
+        # compute subgradient
+        g_t = self.compute_subgradient(self.w, data)
+
+        # update
+        self.update(g_t, h_t, data)
+
+        return self
+
+    def fit(self, data):
+        for i in range(len(data[0])):
+            self.fit_single([data_[i] for data_ in data])
+
+        return self
+
+    @property
+    def losses(self):
+        return np.array(self._losses)
+
+    @property
+    def lin_losses(self):
+        return np.array(self._lin_losses)
+
+
+class OnlineGradientDescent(OnlineConvexOptimizer):
+    def __init__(self, dim, lr_scale, init_w=None, problem=None):
+        super().__init__(dim, problem, init_w)
+
+        # hyperparameters
+        self.lr_scale = lr_scale
+
+        # initialize variables
+        self.g_prev = None
+
+    def lr(self, t):
+        # time varying learning rate
+        return self.lr_scale / np.sqrt(t)
+
+    def get_action(self, h_t=None):
+        t = len(self._losses) + 1
+        if t > 1:
+            assert self.g_prev is not None
+            return self.w - self.lr(t) * self.g_prev  # no projection since unconstrained
+        else:
+            return self.w
+
+    def update(self, g_t, h_t=None, data=None):
+        super().update(g_t, h_t, data)
+        self.g_prev = g_t
+        return self
+
+
+class DimensionFreeExponentiatedGradient(OnlineConvexOptimizer):
+    def __init__(self, dim, a=1, L=1, delta=1, init_w=None, problem=None):
+        """
+        Dimension-free Exponentiated Gradient (DFEG)
+
+        References
+        ----------
+        [1] ...
+
+        Parameters
+        ----------
+        dim
+        a
+        delta
+        L: Lipschitz constant
+        problem
+        """
+        super().__init__(dim, problem, init_w)
+
+        # hyperparameters
+        self.a = a
+        self.delta = delta
+        self.L = L
+
+        # initialize variables
+        self.th = self.init_w.copy()
+        self.H = self.delta
+
+    def reset_stats(self):
+        super().reset_stats()
+        self.th = self.init_w.copy()
+        self.H = self.delta
+        return self
+
+    def get_action(self, h_t=None):
+        # Set parameters
+        self.H += self.L ** 2  # assumed ||x_t|| = 1; TODO: check if this requires to set normalize=True
+        alpha = self.a * np.sqrt(self.H)
+        beta = self.H ** (3 / 2)
+
+        # Compute w
+        norm_th = np.linalg.norm(self.th)
+        if norm_th == 0:
+            w = np.zeros(self.dim)
+        else:
+            w = (self.th / norm_th) * (np.exp(norm_th / alpha) / beta)  # the EG step
+
+        return w
+
+    def update(self, g_t, h_t=None, data=None):
+        super().update(g_t, h_t, data)
+        self.th -= g_t
+        return self
+
+
+class AdaptiveNormal(OnlineConvexOptimizer):
+    def __init__(self, dim, a=1, L=1, eps=1, init_w=None, problem=None):
+        """
+        Adaptive Normal (AdaNormal)
+
+        References
+        ----------
+        [1] ...
+
+        Parameters
+        ----------
+        dim
+        a
+        L: Lipschitz constant
+        eps
+        problem
+        """
+        super().__init__(dim, problem, init_w)
+
+        # hyperparameters
+        self.a = a
+        self.eps = eps
+        self.L = L
+
+        # initialize variables
+        self.th = self.init_w.copy()
+
+    def reset_stats(self):
+        super().reset_stats()
+        self.th = self.init_w.copy()
+        return self
+
+    def get_action(self, h_t=None):
+        t = len(self._losses) + 1
+
+        # Set action
+        norm_th = np.linalg.norm(self.th)
+        if norm_th == 0:
+            w = np.zeros(self.dim)
+        else:
+            term1 = np.exp(((norm_th + self.L) ** 2) / (2 * self.a * (t + 1)))
+            term1 -= np.exp(((norm_th - self.L) ** 2) / (2 * self.a * (t + 1)))
+            term2 = (2 * self.L * (np.log(t + 2)) ** 2) ** (-1)
+            w = self.th * self.eps * term1 * term2 / norm_th  # the AdaNormal step
+
+        return w
+
+    def update(self, g_t, h_t=None, data=None):
+        super().update(g_t, h_t, data)
+        self.th -= g_t
+        return self
+
+
+class CoinBetting(OnlineConvexOptimizer):
+    def __init__(self, dim, init_wealth=1, quantizer=None, init_w=None, problem=None):
+        """
+        A placeholder for coin betting with side information
+
+        Parameters
+        ----------
+        init_wealth
+        quantizer
+        """
+        super().__init__(dim, problem, init_w)
+
+        # hyperparameter
+        self.init_wealth = init_wealth
+        self.quantizer = quantizer
+
+        # initialize parameters
+        self._log_potential = 0
+
+    def reset_stats(self):
+        super().reset_stats()
+        self._log_potential = 0
+        return self
+
+    def get_vector_betting(self, h_t):
+        raise NotImplementedError
+
+    def get_action(self, h_t=None):
+        return self.init_w + self.get_vector_betting(h_t) * self.wealth
+
+    @property
+    def wealth(self):
+        return self.init_wealth + self.cum_reward
+
+    @property
+    def log_potential(self):
+        return self._log_potential
+
+
+class KTCoinBetting(CoinBetting):
+    def __init__(self, dim, init_wealth=1, quantizer=None, init_w=None, problem=None):
+        super().__init__(dim, init_wealth=init_wealth, quantizer=quantizer, init_w=init_w, problem=problem)
+
+        # internal statistics
+        self.counter = defaultdict(int)
+        self.g_cum = defaultdict(lambda: np.zeros(dim))  # cumulative gradients
+
+    def reset_stats(self):
+        super().reset_stats()
+        self.counter = defaultdict(int)
+        self.g_cum = defaultdict(lambda: np.zeros(self.dim))
+        return self
+
+    def get_vector_betting(self, h_t):
+        return self.g_cum[h_t] / (self.counter[h_t] + 1)  # vectorial KT betting
+
+    def update(self, g_t, h_t=None, data=None):
+        super().update(g_t, h_t, data)
+        # update log_potential, counter, cumulative gradient
+        self.counter[h_t] += 1
+        self.g_cum[h_t] += -g_t  # accumulate -g since we are in a loss minimization framework
+        self._log_potential = self.compute_log_kt_potential(h_t)  # \psi(g^{t}(h_t))
+        return self
+
+    def compute_log_kt_potential(self, h_t):
+        # return log KT potential with side information
+        t = self.counter[h_t]
+        x = np.sqrt((self.g_cum[h_t] ** 2).sum())  # l2 norm of \sum g^{t-1}(h_t)
+        return t * np.log(2) + betaln((t + x + 1) / 2, (t - x + 1) / 2) - betaln(.5, .5)
+
+
+class KTCoinBettingWithMarkovSideInformation(KTCoinBetting):
+    def __init__(self, dim, init_wealth=1, depth=1, quantizer=None, init_w=None, problem=None):
+        """
+        Implement coin betting with a fixed order Markov type side information with a binary quantizer
+
+        Parameters
+        ----------
+        depth
+        """
+        super().__init__(dim, init_wealth, quantizer, init_w, problem=problem)
+
+        # hyperparameter
+        self.depth = depth
+
+        # internal statistics
+        self.suffix = [1 for _ in range(self.depth)]  # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]]
+
+    def reset_stats(self):
+        super().reset_stats()
+        self.suffix = [1 for _ in range(self.depth)]  # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]]
+        return self
+
+    def get_side_information(self, data=None):
+        return tuple(self.suffix)  # h_t = Q(g_{t-1}^{t-D})
+
+    def update(self, g_t, h_t=None, data=None):
+        super().update(g_t, h_t, data)
+        suffix = self.suffix[1:] + [self.quantizer(g_t)]  # update the suffix
+        self.suffix = suffix[:self.depth]  # to handle the degenerate case when depth=0
+        return self
+
+
+class KTCoinBettingWithHint(KTCoinBetting):
+    def __init__(self, dim, init_wealth=1, quantizer=None, init_w=None, problem=None):
+        """
+        Coin betting with a fixed order Markov type side information with a binary quantizer
+        """
+        super().__init__(dim, init_wealth, quantizer, init_w, problem=problem)
+
+    def get_side_information(self, data=None):
+        return self.quantizer(self.compute_subgradient(self.w, data))
+
+
+class ContextTreeWeighting(CoinBetting):
+    def __init__(self, dim, init_wealth=1, max_depth=1, alpha=.5, quantizer=None, init_w=None, problem=None):
+        super().__init__(dim, init_wealth, quantizer, init_w, problem=problem)
+
+        # hyperparameters
+        self.alpha = alpha
+        self.max_depth = max_depth
+
+        # internal statistics
+        self.context_tree = ContextTree(max_depth=self.max_depth, alpha=self.alpha, dim=dim)
+        self.suffix = [1 for _ in range(self.max_depth)]  # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]]
+
+    def reset_stats(self):
+        super().reset_stats()
+        self.context_tree = ContextTree(max_depth=self.max_depth, alpha=self.alpha, dim=self.dim)
+        self.suffix = [1 for _ in range(self.max_depth)]  # initialize quantized suffix [Q(g_{t-d}) for d in [1,...,D]]
+        return self
+
+    def get_side_information(self, data=None):
+        return tuple(self.suffix)  # h_t = Q(g_{t-1}^{t-D})
+
+    def get_vector_betting(self, suffix):
+        return self.context_tree.v_ctw(state=suffix)
+
+    def update(self, g_t, h_t=None, data=None):
+        super().update(g_t, h_t, data)
+        self.context_tree.update(state=h_t, g_new=-g_t)
+        suffix = self.suffix[1:] + [self.quantizer(g_t)]  # update the suffix
+        self.suffix = suffix[:self.max_depth]  # to handle the degenerate case when max_depth=0
+        return self
+
+    @property
+    def log_potential(self):
+        return self.context_tree.log_potential
+
+
+class CombineAlgorithms(OnlineConvexOptimizer):
+    def __init__(self, dim, algorithms):
+        super().__init__(dim, algorithms[0].problem)
+        self.algorithms = algorithms
+        self.reset_stats()
+
+    def reset_stats(self):
+        for algorithm in self.algorithms:
+            algorithm.reset_stats()
+        return self
+
+    def get_side_information(self, data=None):
+        h_ts = []
+        for i, algorithm in enumerate(self.algorithms):
+            h_t = algorithm.get_side_information(data)
+            h_ts.append(h_t)
+        return h_ts
+
+    def get_action(self, h_ts=None):
+        raise NotImplementedError
+
+    def update(self, g_t, h_ts=None, data=None):
+        super().update(g_t, h_ts, data)
+        # update individual algorithms
+        for i, algorithm in enumerate(self.algorithms):
+            algorithm.update(g_t, h_ts[i], data)  # update losses based on each algorithm's action
+            # Note: Addition depends on the losses of the constituent algorithms, but
+            #       Mixture only requires each algorithm to maintain side information correctly
+        return self
+
+
+class Addition(CombineAlgorithms):
+    def __init__(self, dim, algorithms):
+        super().__init__(dim, algorithms)
+
+    def get_action(self, h_ts=None):
+        w = np.zeros(self.dim)
+        for i, algorithm in enumerate(self.algorithms):
+            # the correct implementation
+            algorithm.w = algorithm.get_action(h_ts[i])  # set action for each algorithm
+            w += algorithm.w
+
+        return w
+
+
+class AddingVectorBettings(CombineAlgorithms):
+    def __init__(self, dim, algorithms, init_wealth=1):
+        super().__init__(dim, algorithms)
+        for algorithm in algorithms:
+            assert hasattr(algorithm, 'get_vector_betting')
+
+        self.init_wealth = init_wealth
+
+    def get_vector_betting(self, h_ts=None):
+        v = np.zeros(self.dim)
+        for i, algorithm in enumerate(self.algorithms):
+            # TODO: somehow it works very well?
+            v += algorithm.get_vector_betting(h_ts[i])
+
+        return v
+
+    @property
+    def wealth(self):
+        return self.init_wealth  # + self.cum_reward
+
+    def get_action(self, h_ts=None):
+        return self.init_w + self.wealth * self.get_vector_betting(h_ts)
+
+
+class Mixture(CombineAlgorithms):
+    def __init__(self, dim, algorithms, init_wealth=1, weights=None):
+        super().__init__(dim, algorithms)
+        for algorithm in algorithms:
+            assert hasattr(algorithm, 'log_potential')
+            assert hasattr(algorithm, 'get_vector_betting')
+
+        self.init_wealth = init_wealth
+        if weights is None:
+            weights = np.ones(len(self.algorithms))
+        self.log_weights = np.log(weights)
+
+    def get_vector_betting(self, h_ts=None):
+        vs = []
+        log_weighted_potentials = []  # log(w_m * psi_m)
+
+        for i, algorithm in enumerate(self.algorithms):
+            # v = algorithm.get_vector_betting(h_ts[i])
+            # algorithm.w = algorithm.wealth * v
+            # vs.append(v)
+            vs.append(algorithm.get_vector_betting(h_ts[i]))
+            log_weighted_potentials.append(self.log_weights[i] + algorithm.log_potential)
+
+        log_potential_mixture = logsumexp(log_weighted_potentials)
+        weights = np.exp(np.array(log_weighted_potentials) - log_potential_mixture)
+        v_mixture = np.sum(weights[:, np.newaxis] * np.array(vs), axis=0)
+        # v_mixture = np.sum(np.array(vs), axis=0) / len(self.algorithms)
+        return v_mixture
+
+    @property
+    def wealth(self):
+        return self.init_wealth + self.cum_reward
+
+    def get_action(self, h_ts=None):
+        return self.init_w + self.wealth * self.get_vector_betting(h_ts)
